f035df92dbc7fe4303e0248037b6dc04a5b87b51
mkosova-alija <59917953+mkosova-alija@users.noreply.github.com>
2024-11-15 15:25:33 +0200

    Update news.py

--- news.py
+++ news.py
@@ -3,7 +3,7 @@ from newsdataapi import NewsDataApiClient
 
 
 # API key authorization, Initialize the client with your API key
-api = NewsDataApiClient(apikey='pub_30503bd51e9cacdfa6a40fa77f953a582632c')
+api = NewsDataApiClient(apikey='pub_*********')
 
 
 # News API
